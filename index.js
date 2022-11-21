e028487e5329b983c2b154e959fd661163b56c1f
Szymon Ludziejewski <szymon.ludziejewski@vizlib.com>
2022-11-21 16:59:37 +0100

    chore: add swagger samples, add nodemon

--- index.js
+++ index.js
@@ -7,9 +7,36 @@ const environment = process.env;
 const app = express();
 app.use(express.json());
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     DefaultResponse:
+ *      type: object
+ *      properties:
+ *       desc:
+ *        type: string
+ *        description: Short description to current path
+ *      example:
+ *        desc: A description
+ */
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Return the main path of the app
+ *     responses:
+ *       200:
+ *         description: Returns description where to see the documentation
+ *         content: 
+ *          application/json:
+ *           schema:
+ *            $ref: '#/components/schemas/DefaultResponse'
+ */
 app.get('/', (_request, response) => {
     response.json({
-        description:
+        desc:
             'Type /api-docs on the end of URL to see the SWAGGER documentation',
     });
 });
--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,24 @@ const express = require('express');
 const router = express.Router();
 const database = require('../simple-database/database');
 
+
+/**
+ * @swagger
+ * /api:
+ *   get:
+ *     summary: Return description of a task for students
+ *     responses:
+ *       200:
+ *         description: Return description of a task for students
+ *         content: 
+ *          application/json:
+ *           schema:
+ *            $ref: '#/components/schemas/DefaultResponse'
+ */
 router.get('/', (_request, response) => {
     response.json({
-        description:
-            'Zaimplmentuj ścieżki /students /employees /faculty z użyciem metod GET, POST, PUT, PATCH, DELETE',
+        desc:
+            'Implement a documentation to the existing endpoints',
     });
 });
 
